c68337b70b07409f7ca5fbdf10925adce0b986e6
Kjell Andreassen <andreassen.kjell@gmail.com>
2015-11-24 10:59:03 +0100

    Some kind of bash history file parsing. Merge/sort broken.

---
+++ parse-bash-hist.hs
@@ -0,0 +1,54 @@
+#!/usr/bin/env runhaskell
+
+module Main where
+
+import Data.List
+import System.Environment
+import Text.ParserCombinators.Parsec
+
+main :: IO ()
+main = do
+  args <- getArgs
+  ss <- mapM readFile args
+  let ps = mapM sortHist $ zip args ss
+  let res = foldr merge [] ps
+  mapM_ printHist res
+
+printHist (i, c) = do
+  putChar '#'
+  print i
+  putStrLn c
+
+sortHist (f, s) =
+  case parseFile f s of
+    Left e  -> [(0, "Error parsing input:" ++ show e)]
+    Right r -> sort r
+
+parseFile :: FilePath -> String -> Either ParseError [(Int, String)]
+parseFile = parse bashHistoryFile
+
+bashHistoryFile = many histElem
+
+histElem :: GenParser Char st (Int, String)
+histElem = do
+  num <- histTime
+  cmd <- histCmd
+  return (read num :: Int, cmd)
+
+histTime = do
+  char '#'
+  num <- many1 digit
+  newline
+  return num
+
+histCmd = do
+  cmd <- many1 (noneOf "\n")
+  newline
+  return cmd
+
+merge :: Ord a => [a] -> [a] -> [a]
+merge [] ys = ys
+merge xs [] = xs
+merge (x:xs) (y:ys)
+  | x <= y    = x : merge xs (y:ys)
+  | otherwise = y : merge (x:xs) ys
